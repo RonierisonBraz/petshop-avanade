9a656f565bdb60286d0988e575d64c9166ef940c
RonierisonBraz <ronierison18@gmail.com>
2021-03-25 14:12:23 -0300

    foi adicionado 3 funções: vacinarPet, vacinacaoPets, adicionarPet

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const nomePetshop = " PETSHOP AVANADE" ;
 
-let pet = [{
+let pets = [{
     nome: 'Melissa',
     tipo: 'cachorro',
     idade: 4,
@@ -8,7 +8,7 @@ let pet = [{
     peso: 6,
     tutor: 'Thais',
     conntato: '(81) 98888-8888',
-    vacinado: true,
+    vacinado: false,
     serviços: ['banho', 'tosa']
 },
 {
@@ -19,7 +19,7 @@ let pet = [{
     peso: 6,
     tutor: 'Thais',
     conntato: '(81) 98888-8888',
-    vacinado: true,
+    vacinado: false,
     serviços: ['banho', 'tosa']
 },
 {
@@ -30,18 +30,73 @@ let pet = [{
     peso: 6,
     tutor: 'Thais',
     conntato: '(81) 98888-8888',
-    vacinado: true,
+    vacinado: false,
     serviços: ['banho', 'tosa']
 }
 ];
 
 const listarPets = () => {
-     for(let pets of pet){
-        console.log(`${pets.nome},${pets.idade},${pets.tipo},${pets.raca}`); 
+     for(let pet of pets){
+        console.log(`${pet.nome},${pet.idade},${pet.tipo},${pet.raca}`); 
      }
 }
 
 listarPets();
 
-// console.log(pet);
+const vacinarPet = (pet) => {
+    if (pet.vacinado === true){
+        console.log(`${pet.nome} já está vacinado`);
+    }else{
+        pet.vacinado = true;
+        console.log(`${pet.nome} foi vacinado com sucesso. `);
+    }
+}
 
+const vacinacaoPets = () => {
+    totalVacinados = 0;
+    for(let pet of pets){
+        if (pet.vacinado === false){
+            pet.vacinado = true;
+            totalVacinados++;
+        }
+    }
+    console.log(`${totalVacinados} animais foram vacinados nessa campannha.`)
+}
+
+const adicionarPet = (nomepet, tipoPet, idadePet, racaPet, pesoPet, tutorPeT, vacinadoPet, servircosPet ) => {
+    novoPet = {
+        nomepet, 
+        tipoPet, 
+        idadePet, 
+        racaPet, 
+        pesoPet, 
+        tutorPeT, 
+        vacinadoPet, 
+        servircosPet 
+    }
+    pets.push(novoPet);
+    console.log(pets);
+
+}
+
+adicionarPet('salen', 'gato', 7, 'bruxo', 7, 'thamires', '81 9999-9999', true, '');
+
+
+// const adicionarPet = () => {
+//     novoPet = {
+//         nome: 'salen',
+//         tipo: 'gato',
+//         idade: 7,
+//         raca: 'Vira-lata',
+//         peso: 6,
+//         tutor: 'Thais',
+//         conntato: '(81) 98888-8888',
+//         vacinado: false,
+//         serviços: ['banho', 'tosa']
+//     };
+//     pets.push(novoPet);
+//     console.log(pets);
+// }
+
+// vacinarPet(pets[2]);
+// vacinacaoPets(pets);
